262189685806e7186e2e5ba9d9dc98e11bab3d73
Akhil <83167088+simplyakh1l@users.noreply.github.com>
2026-02-22 20:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -6,12 +6,15 @@ public:
 
         if(n<=1)return 1;
 
-        dp[0]=dp[1]=1;
-
+        int p1=1,p2=1;
+        int cur;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            cur=p1+p2;
+            p2=p1;
+            p1=cur;
         }
-        return dp[n];
+        return p1;
+        
         
         
     }
